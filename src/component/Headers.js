0b3beeee5c330456cd77839dd078c3acf6f574a7
andrea torres <andreamelissa.tc@gmail.com>
2023-02-22 14:31:53 -0500

    a

--- src/component/Headers.js
+++ src/component/Headers.js
@@ -1,11 +1,10 @@
 import React from "react";
-import FontAwesomeIcon from "fontawesome";
+
 
 export default function Headers() {
   return (
     <header>
       <nav className="nav">
-      <FontAwesomeIcon icon="fa-solid fa-chalkboard" />
         <h4 className="an">A&N</h4>
       <ol className="nav-items">
         <li>About</li>
